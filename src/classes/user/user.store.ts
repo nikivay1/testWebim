b5668785ee1c76a95a559788d55a6a5c356ea8a3
Nikita Veysman <veysmanns@it2g.ru>
2021-01-20 00:56:27 +0300

    Delete user Observable

--- src/classes/user/user.store.ts
+++ src/classes/user/user.store.ts
@@ -2,21 +2,9 @@ import { Injectable } from '@angular/core';
 import { Observable, of, BehaviorSubject } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { ApiService } from 'src/api/api.service';
-import { User } from './user';
 
 @Injectable({providedIn: 'root'})
 export class UserStorage {
-
-    private readonly _user:BehaviorSubject<User> = new BehaviorSubject<User>(null);
-    readonly user$:Observable<User> = this._user.asObservable();
-
-    get user():User{
-        return this._user.getValue()
-    }
-
-    set user(user:User){
-        this._user.next(user)
-    }
 
     get isLoggedIn():boolean{
         return this._api.cookie.check('token')
@@ -43,7 +31,6 @@ export class UserStorage {
     }
     
     public userLogout() {    
-        this.user = null
         this._api.cookie.delete('token');
     }
 
